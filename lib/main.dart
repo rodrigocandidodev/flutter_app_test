f7e486592a3706260a5b65fcaf33f3ceb9434f5e
Rodrigo Candido da Silva <rodrigo_candido.contato@outlook.com>
2026-01-12 21:45:33 -0300

    Organize the files

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app_widget.dart';
 
 void main() {
   runApp(AppWidget(title: "Flutter app"));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) { 
-    return MaterialApp(
-      theme: ThemeData(primarySwatch:  Colors.red),
-      home: HomePage(title)
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  final String title;
-
-  const HomePage(this.title, {super.key});
-
-  @override
-  State<HomePage> createState() {
-    return HomePageState(title);
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  final String title;
-  int count = 0;
-
-  HomePageState(this.title);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(title),
-            GestureDetector(
-              child: Text("Counter: $count"),
-              onTap: () { 
-                setState(() {
-                  count ++;
-                });
-              }
-            )
-          ]
-        )
-    );
-  }
 }
